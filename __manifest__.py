4e5fe5cd699c0d5f14555cf6d445740cd3197842
Gabriel Bonilla <gpbonillas@gmail.com>
2023-10-27 10:12:03 -0500

    Add modules sale and account to dependencies

--- __manifest__.py
+++ __manifest__.py
@@ -14,7 +14,7 @@
     'version': '1.0',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'mail'],
+    'depends': ['base', 'mail', 'sale', 'account'],
 
     # always loaded
     'data': [
